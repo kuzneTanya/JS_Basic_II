f473dba83575e4cb3164cc1f69470b00abe0c1d6
Tatyana Kuznetsova <kuznetsova.t.s.nmc@gmail.com>
2024-11-11 16:24:23 +0500

    task 10 done

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -1,12 +1,17 @@
-
-window.onload = function()
-{
+document.querySelector("#generate").addEventListener('click', function() {
     const initPerson = personGenerator.getPerson();
     document.querySelector('#surnameOutput').innerHTML = initPerson.surname;
     document.querySelector('#firstNameOutput').innerHTML = initPerson.firstName;
-    document.querySelector('#genderOutput').innerHTML = initPerson.gender;
+    document.querySelector('#genderOutput').innerHTML = initPerson.gender + ',';
     document.querySelector('#birthDateOutput').innerHTML = initPerson.dateOfBirth;
     document.querySelector('#lastNameOutput').innerHTML = initPerson.lastName;
     document.querySelector('#jobOutput').innerHTML = initPerson.job;
-};
+})
 
+document.querySelector("#clear").addEventListener('click', function() {
+    let spans = document.querySelectorAll('span');
+    for (i=0; i<spans.length; i++) {
+        spans[i].innerHTML = "";
+    };
+    document.querySelector("#surnameOutput").innerHTML = 'Нажмите кнопку "Сгенерировать", чтобы получить данные';
+})
